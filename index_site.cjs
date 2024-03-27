a4292e43038f0dc4384377919aaee9642c82432d
nothimofc <hkhan7@ualberta.ca>
2024-03-27 00:35:13 -0600

    90% finished

--- index_site.cjs
+++ index_site.cjs
@@ -12,6 +12,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/generate", (req, res) => {
+  const size = parseInt(req.query.size) || 99  ; // Get the size from the query parameter or default to 51
+
   // Delete existing maze.txt and path.txt files
   const deleteFile = (filePath) => {
     if (fs.existsSync(filePath)) {
@@ -22,7 +24,7 @@ app.get("/generate", (req, res) => {
   deleteFile("maze.txt");
   deleteFile("path.txt");
 
-  const maze = generator.data.genMaze();
+  const maze = generator.data.genMaze(size);
   fs.writeFile("maze.txt", maze, (err) => {
     if (err) throw err;
     res.send(maze);
@@ -30,12 +32,13 @@ app.get("/generate", (req, res) => {
 });
 
 app.get("/solve", (req, res) => {
-  readMaze((solvedMaze) => {
+  const size = parseInt(req.query.size) || 99 ; // Get the size from the query parameter or default to 51
+  readMaze(size, (solvedMaze) => {
     res.send(solvedMaze);
   });
 });
 
-function readMaze(callback) {
+function readMaze(size, callback) {
   if (fs.existsSync("maze.txt")) {
     var maze = [];
     fs.readFile("maze.txt", "utf-8", (err, data) => {
@@ -55,30 +58,32 @@ function readMaze(callback) {
       maze.push(array);
       var test = function (err) {
         if (err) throw err;
-        fs.readFile("path.txt", "utf-8", (err, pathData) => {
+        const path = dijkstra.data.dijkstraCalculateMaze(
+          maze,
+          { x: 0, y: 1 },
+          { x: size - 1, y: size - 2 },
+          astar.data.getWeight
+        );
+        const pathString = path.map(coord => `${coord.x},${coord.y}`).join('\n');
+        fs.writeFile("path.txt", pathString, (err) => {
           if (err) throw err;
-          const path = pathData.split("\n").map((row) => row.split(""));
-          for (let i = 0; i < maze.length; i++) {
-            for (let j = 0; j < maze[i].length; j++) {
-              if (path[i] && (path[i][j] === "S" || path[i][j] === "E" || path[i][j] === "P")) {
-                maze[i][j] = path[i][j];
-              }
+          for (let i = 0; i < path.length; i++) {
+            const { x, y } = path[i];
+            if (x >= 0 && x < maze.length && y >= 0 && y < maze[x].length) {
+              maze[x][y] = "P";
             }
+          }
+          if (maze[0] && maze[0][1]) {
+            maze[0][1] = "S";
+          }
+          if (maze[size - 1] && maze[size - 1][size - 2]) {
+            maze[size - 1][size - 2] = "E";
           }
           const solvedMaze = maze.map((row) => row.join("")).join("\n");
           callback(solvedMaze);
         });
       };
-      fs.writeFile(
-        "path.txt",
-        dijkstra.data.dijkstraCalculateMaze(
-          maze,
-          { x: 0, y: 1 },
-          { x: 50, y: 49 },
-          astar.data.getWeight
-        ),
-        (err) => test(err)
-      ); // calculate best path
+      test();
     });
   } else {
     callback("Maze not found. Please generate a maze first.");
